3e78e2fc876db269f585dc122702f034fa85fe45
Varinder Singh <varindersingh6885@gmail.com>
2024-09-09 22:01:02 +0530

    feat: add bank-webhook route to update balance and onRampTransaction

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import db from "@repo/db/client";
+
+const app = express();
+
+app.post("/hdfcWebhook", async (req, res) => {
+  //TODO: Add zod validation here?
+  const paymentInformation = {
+    token: req.body.token,
+    userId: req.body.user_identifier,
+    amount: req.body.amount,
+  };
+
+  try {
+    await db.$transaction([
+      db.balance.update({
+        where: {
+          userId: paymentInformation.userId,
+        },
+        data: {
+          amount: {
+            increment: paymentInformation.amount,
+          },
+        },
+      }),
+      db.onRampTransaction.update({
+        where: {
+          token: paymentInformation.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+
+    res.json({
+      message: "Captured",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(411).json({
+      message: "Error while processing payment",
+    });
+  }
+});
